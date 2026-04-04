6f94dcd2ac373bf402943ce50de74ebd3005e95e
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-04-04 22:49:27 +0530

    Added Patter Printing Cpp Assignment Programs

---
+++ Assignments_22/Program22_05.cpp
@@ -0,0 +1,92 @@
+/* 
+    5. Accept number of rows and number of columns from user and display below pattern.
+
+    Input  : iRow = 4 iCol = 4
+
+    Output :    1   2   3   4
+                2   3   4   5
+                3   4   5   6
+                4   5   6   7
+*/
+
+#include<iostream>
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name       : Pattern
+//  Description      : Contains function to print increasing numbers in each row starting from row number using rows and columns.
+//  Input            : Integer values (Row and Column)
+//  Output           : Prints pattern on screen
+//  Author           : Gavade Somanath Vilas
+//  Date             : 04/04/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public:
+        int iRow;
+        int iCol;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Constructor Name : Pattern (Parameterized Constructor)
+//  Description      : Initializes data members with given values.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        Pattern(int A, int B)
+        {
+            iRow = A;
+            iCol = B;
+        }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name    : Display
+//  Description      : Prints increasing numbers in each row starting from row number.
+//  Input            : None (Uses class data members)
+//  Output           : Prints pattern on screen
+//  Time Complexity  : O(N^2)
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void Display()
+        {
+            int i = 0, j = 0;
+
+            for(i = 1; i <= iRow; i++)
+            {
+                for(j = 0; j < iCol; j++)
+                {
+                    cout << i + j << "\t";
+                }
+                cout << "\n";
+            }
+        }
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : Row and Column increasing number pattern printing using OOP
+//  Description      : This application accepts number of rows and columns from user, creates an object of Pattern class and prints increasing number pattern.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout << "Enter number of Rows : \n";
+    cin >> iValue1;
+
+    cout << "Enter Number of Coloumns : \n";
+    cin >> iValue2;
+
+    Pattern pobj(iValue1, iValue2);
+
+    pobj.Display();
+
+    return 0;
+}
