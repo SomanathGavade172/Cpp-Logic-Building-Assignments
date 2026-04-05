90041853bd0044e44c67546eaf3c4ebe44b5d13a
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-04-05 23:15:43 +0530

    Added Patter Printing Cpp Assignment Programs

---
+++ Assignments_23/Program23_01.cpp
@@ -0,0 +1,104 @@
+/* 
+    1. Accept number of rows and number of columns from user and display below pattern.
+
+    Input  : iRow = 4 iCol = 4
+
+    Output :    
+                * # # #
+                * * # #
+                * * * #
+                * * * *
+
+*/
+
+#include<iostream>
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name       : Pattern
+//  Description      : Contains function to print pattern of * and # using rows and columns.
+//  Input            : Integer values (Row and Column)
+//  Output           : Prints pattern on screen
+//  Author           : Gavade Somanath Vilas
+//  Date             : 05/04/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public:
+        int iRow;
+        int iCol;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Constructor Name : Pattern (Parameterized Constructor)
+//  Description      : Initializes data members with given values.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        Pattern(int A, int B)
+        {
+            iRow = A;
+            iCol = B;
+        }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name    : Display
+//  Description      : Prints pattern where * appears when column number is less than or equal to row number
+//                     otherwise # is printed.
+//  Input            : None (Uses class data members)
+//  Output           : Prints pattern on screen
+//  Time Complexity  : O(N^2)
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void Display()
+        {
+            int i = 0, j = 0;
+
+            for(i = 1; i <= iRow; i++)
+            {
+                for(j = 1; j <= iCol; j++)
+                {
+                    if(i < j)
+                    {
+                        cout << "#\t";
+                    }
+                    else if(i >= j)
+                    {
+                        cout << "*\t";
+                    }
+                }
+                cout << "\n";
+            }
+        }
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : Row and Column pattern printing using OOP
+//  Description      : This application accepts number of rows and columns from user, creates an object of 
+//                     Pattern class and prints * and # pattern.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout << "Enter number of Rows : \n";
+    cin >> iValue1;
+
+    cout << "Enter Number of Coloumns : \n";
+    cin >> iValue2;
+
+    Pattern pobj(iValue1, iValue2);
+
+    pobj.Display();
+
+    return 0;
+}
