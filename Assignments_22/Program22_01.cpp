46497c5f4c114ba05de3dad47c67dddbc5d90488
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-07-04 22:04:04 +0530

    Added Patter Printing Cpp Assignment Programs

---
+++ Assignments_22/Program22_01.cpp
@@ -0,0 +1,95 @@
+/* 
+    1. Accept number of rows and number of columns from user and display below pattern.
+
+    Input  : iRow = 4 iCol = 4
+
+    Output :    
+                1   2   3   4
+                5   6   7   8
+                9   1   2   3
+                4   5   6   7
+*/
+
+#include<iostream>
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name       : Pattern
+//  Description      : Contains function to print numbers from 1 to 9 repeatedly using rows and columns.
+//  Input            : Integer values (Row and Column)
+//  Output           : Prints pattern on screen
+//  Author           : Gavade Somanath Vilas
+//  Date             : 04/04/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public:
+        int iRow;
+        int iCol;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Constructor Name : Pattern (Parameterized Constructor)
+//  Description      : Initializes data members with given values.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        Pattern(int A, int B)
+        {
+            iRow = A;
+            iCol = B;
+        }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name    : Display
+//  Description      : Prints numbers sequentially and repeats after 9.
+//  Input            : None (Uses class data members)
+//  Output           : Prints pattern on screen
+//  Time Complexity  : O(N^2)
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void Display()
+        {
+            int i = 0, j = 0, iCnt = 1;
+
+            for(i = 1; i <= iRow; i++)
+            {
+                for(j = 1; j <= iCol; j++)
+                {
+                    cout << iCnt << "\t";
+
+                    iCnt = (iCnt % 9) + 1;
+                }
+                cout << "\n";
+            }
+        }
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : Row and Column repeating number pattern printing using OOP
+//  Description      : This application accepts number of rows and columns from user, creates an object of Pattern class and prints repeating number pattern.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    cout << "Enter number of Rows : \n";
+    cin >> iValue1;
+
+    cout << "Enter Number of Coloumns : \n";
+    cin >> iValue2;
+
+    Pattern pobj(iValue1, iValue2);
+
+    pobj.Display();
+
+    return 0;
+}
